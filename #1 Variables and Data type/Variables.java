cf8761f458ec2d8a2d13ec6423391c5f868e72d9
Jayeshpatidar24 <92778917+Jayeshpatidar24@users.noreply.github.com>
2025-01-08 16:57:25 +0530

    #1

---
+++ #1 Variables and Data type/Variables.java
@@ -0,0 +1,24 @@
+public class Variables{
+  public static void main(String[] args) {
+  
+// varibales are containers where data stored
+// int is data type var is variable name and 5 is value or data
+int var = 5;
+//types of data types 
+
+//primitive data types
+byte b = 5;
+short sh =6;
+int i = 7;
+float f = 6.0f;
+double d = 5666.0d;
+char c = 'a';
+boolean bool = true;
+
+//non-primitive data types
+//array, string, classes, interfaces, enum
+
+int arr
+
+  }
+}
